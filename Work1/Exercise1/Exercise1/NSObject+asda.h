aee603419b7c1560af00c03d55cdcc46a15827ac
Mayer Vladimir <vmayer2002@bk.ru>
2024-06-13 15:37:46 +0300

    test commit

---
+++ Work1/Exercise1/Exercise1/NSObject+asda.h
@@ -0,0 +1,16 @@
+//
+//  NSObject+asda.h
+//  Exercise1
+//
+//  Created by Владимир Майер on 13.06.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSObject ()
+
+@end
+
+NS_ASSUME_NONNULL_END
